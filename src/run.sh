b6d59d122fbb570d223e89ba895857d323ade870
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2022-07-21 16:23:59 -0400

    Create run.sh

---
+++ src/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --gres=gpu:1
+#SBATCH --mem=10G
+
+module purge
+conda activate /home/x/xl456/miniconda3_1/envs/torch_py38
+
+f=./realdata/sample_151507_anno.h5
+
+python -u run_stGAE.py --n_clusters -1 --data_file $f --save_dir sample_151507 --knn 20 \
+--final_labels pred.csv --final_latent_file latent.csv --run $i \
+--ml_file realdata/sample_151507_mlFromMarks.txt --cl_file realdata/sample_151507_clFromMarks.txt
