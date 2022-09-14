70eac2647afc352826bfb15444c7fd284e8570af
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2022-09-14 01:40:48 -0400

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -l
 #SBATCH --gres=gpu:1
-#SBATCH --mem=10G
+#SBATCH --mem=20G
+#SBATCH -J dssctest
 
 module purge
 conda activate /home/x/xl456/miniconda3_1/envs/torch_py38
 
-f=./realdata/sample_151507_anno.h5
-fs = ./realdata/sample_151507_featureSelection.txt
+f=./sample_data/sample_151507_anno.h5
+fs=./sample_data/sample_151507_featureSelection_Index2000.csv
 
-python -u run_stGAE.py --n_clusters -1 --data_file $f --FS_data $fs \
---save_dir sample_151507 --knn 20 \
+python -u run_DSSC.py --n_clusters -1 --data_file $f --FS_file $fs \
+--save_dir out_151507 --knn 20 \
 --final_labels pred.csv --final_latent_file latent.csv --run $i \
---ml_file realdata/sample_151507_mlFromMarks.txt --cl_file realdata/sample_151507_clFromMarks.txt
+--ml_file sample_data/sample_151507_mlFromMarks.txt --cl_file sample_data/sample_151507_clFromMarks.txt
