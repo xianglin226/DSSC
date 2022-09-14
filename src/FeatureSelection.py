a6cfb946cef389367dcb810a8c3a7abf618141eb
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2022-09-14 01:39:53 -0400

    Update FeatureSelection.py

--- src/FeatureSelection.py
+++ src/FeatureSelection.py
@@ -4,13 +4,15 @@ import numpy as np
 from utils import *
 from preprocess0 import read_dataset, normalize
 from sklearn.neighbors import kneighbors_graph
+from joblib import Parallel, delayed
 
 #x is the count matrix; 
 #Loc is the spatial coordinates
 #Here we use the sample 151507 as an example, y is just used to remove unknown cells
-sample = "151507"
+sample = '151507'
+inputfile = './sample_data/sample_' + sample + '_anno.h5'
 n_features = 2000
-data_mat = h5py.File('./sample_' + sample + '_anno.h5')
+data_mat = h5py.File(inputfile)
 x = np.array(data_mat['X'])
 y0 = np.array(data_mat['Y'])
 loc = np.array(data_mat["Pos"])
@@ -32,14 +34,31 @@ adata0 = read_dataset(adata0,transpose=False,test_split=False,copy=True)
 adata0 = normalize(adata0,size_factors=True,normalize_input=True,logtrans_input=True)
 
 #sort and extract features by Moran's I
-score = []
-for i in range(adata0.X.shape[1]):
-      gene = adata0.X[:,i]
-      I = Iscore_gene(gene, A)
-      score.append(I)
-      if i%100==0:
-         print(i)            
+s = np.sum(A)
+N = len(adata0.X[:,0])
+s1 = N/s
+def Iscore_genes(g=np.array([]), A=np.array([[]])):
+    g_= np.mean(g)
+    g_f = g - g_
+    g_f_1 = g_f.reshape(1,-1)
+    g_f_2 = g_f.reshape(-1,1)
+    A_ = np.dot(g_f_2,g_f_1)
+    r = np.sum(A*A_)
+    l = np.sum(g_f**2)
+    s2 = r/l   
+    return s1*s2
+    
+def getindex(i): 
+    if i%100==0:
+       print(i)
+    gene = adata0.X[:,i]
+    I = Iscore_genes(gene, A)
+    return I
+
+score = Parallel(n_jobs=64)(delayed(getindex)(i) for i in range(adata0.X.shape[1]))
+         
 score_ = np.argpartition(score, -n_features)[-n_features:]
 
 #output the index of the HVGs
 np.savetxt("sample_" + sample + "_featureSelection_Index2000.csv", score_, delimiter="\t")
+
