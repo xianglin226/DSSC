5e4747c9b8c4adbc28c422b9953dca93f4e28eda
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2022-10-23 04:31:34 -0400

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -7,11 +7,18 @@ module purge
 conda activate /home/x/xl456/miniconda3_1/envs/torch_py38
 
 f=../sample_data/sample_151507_anno.h5
-
 for i in {1..5}
  do
-python -u run_DSSC.py --n_clusters -1 --data_file $f \
---save_dir out_151507 --knn 20 \
+python -u run_DSSC.py --n_clusters -1 --data_file $f --save_dir out_151507 \
 --final_labels pred.csv --final_latent_file latent.csv --run $i \
 --ml_file sample_151507_mlFromMarks.txt --cl_file sample_151507_clFromMarks.txt
+ done
+
+f=../sample_data/osmFISH_cortex.h5
+for i in {1..5}
+ do
+python -u run_DSSC.py --n_clusters -1 --data_file $f --save_dir out_osmFISH \
+--select_genes 30 --encodeLayer 16 --decodeLayer 16 --z_dim 8 \
+--final_labels pred.csv --final_latent_file latent.csv --run $i \
+--ml_file sample_osmFish_mlFromMarks.txt --cl_file sample_osmFish_clFromMarks.txt
  done
