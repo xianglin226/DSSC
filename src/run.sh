971828d2fbe921e851002c7db812b511daf0bf58
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2022-10-23 04:34:24 -0400

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -11,7 +11,7 @@ for i in {1..5}
  do
 python -u run_DSSC.py --n_clusters -1 --data_file $f --save_dir out_151507 \
 --final_labels pred.csv --final_latent_file latent.csv --run $i \
---ml_file sample_151507_mlFromMarks.txt --cl_file sample_151507_clFromMarks.txt
+--ml_file ../example_constraints/sample_151507_mlFromMarks_test.txt --cl_file ../example_constraints/sample_151507_clFromMarks_test.txt
  done
 
 f=../sample_data/osmFISH_cortex.h5
@@ -20,5 +20,5 @@ for i in {1..5}
 python -u run_DSSC.py --n_clusters -1 --data_file $f --save_dir out_osmFISH \
 --select_genes 30 --encodeLayer 16 --decodeLayer 16 --z_dim 8 \
 --final_labels pred.csv --final_latent_file latent.csv --run $i \
---ml_file sample_osmFish_mlFromMarks.txt --cl_file sample_osmFish_clFromMarks.txt
+--ml_file ../example_constraints/sample_osmFish_mlFromMarks_test.txt --cl_file ../example_constraints/sample_osmFish_clFromMarks_test.txt
  done
